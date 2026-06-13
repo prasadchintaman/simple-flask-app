4a3d87c3e7278cbbf227e27763341ef7d7d5fbee
prasad1993 <prasadbhoyar62@gmail.com>
2026-06-13 16:56:09 +0000

    update code with new changes

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Jenkins Shared Library Project &welcome to DevOps"
+    return "Hello from Jenkins Shared Library Project & welcome to devops batch 11"
 
-app.run(host="0.0.0.0",port=5000)
+app.run(host="0.0.0.0",port=80)
